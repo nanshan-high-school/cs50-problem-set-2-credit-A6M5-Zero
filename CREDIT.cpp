213901fca3f5d9f20dac66886b41c5edfce1a3fa
A6M5-Zero <43640950+A6M5-Zero@users.noreply.github.com>
2018-11-15 19:27:11 +0800

    新增特殊卡號&修bug

--- CREDIT.cpp
+++ CREDIT.cpp
@@ -8,7 +8,7 @@ int main() {
   cout << "Please enter your card number";
   cin >> card;
   account = card;
-  if (card/1000000000000000 == 4) {
+  if (card/1000000000000000 == 4 || card / 1000000000000 == 4) {
     int total = 0;
     int key = card % 10;
     int cnt = 2;
@@ -36,7 +36,7 @@ int main() {
       time++;
     }
     int last_num = 10-total%10;
-    if (last_num == key) {
+    if (last_num == key || last_num == 10) {
       cout << "VISA卡，卡號" << account;
     } else {
       cout << "你不合法喔";
@@ -72,13 +72,49 @@ int main() {
         time++;
       }
       int last_num = 10-total%10;
-      if (last_num == key) {
+      if (last_num == key || last_num == 10) {
         cout << "MASTER卡，卡號" << account;
       } else {
         cout << "你不合法喔";
       }
     }
-  }else {
+  } else if (card / 1000000000000000 == 2) {
+    int test = card / 10000000000;
+    if ( test >= 222100 && test <= 272099) {
+      int total = 0;
+      int key = card % 10;
+      int cnt = 2;
+      for (long int a = 1000000000000000;time <= 15;a = a / 10) {
+        int num = 0;
+        num = card / a;
+        card = card % a;
+        int ten = 0;
+        int one = 0;
+        if (cnt <= 16) {
+          if (cnt % 2 == 0) {
+            if (num * 2 >= 10) {
+              ten = num * 2 / 10;
+              one = num * 2 % 10;
+              total = total + ten + one;
+            } else {
+              total = total + num * 2;
+            }
+            cnt++;
+          } else {
+            total = total + num;
+            cnt++;
+          }
+        }
+        time++;
+      }
+    int last_num = 10-total%10;
+    if (last_num == key || last_num == 10) {
+      cout << "MASTER卡，卡號" << account;
+    } else {
+      cout << "你不合法喔";
+    }
+  } else {
     cout << "你不合法喔";
   }
+}
 }
